7c2f80c19999521865f7126255840eff4a0c05b6
OmKansara <okansara8@gmail.com>
2024-04-21 02:08:04 +0530

    updated app.py

--- app.py
+++ app.py
@@ -10,12 +10,9 @@ app = Flask(__name__)
 app.secret_key = os.environ["SECRET_KEY"]
 app.config["MAX_CONTENT_LENGTH"] = 16 * 1000 * 1000  # 16MB
 
-
 @app.route("/")
 def home():
     return render_template("index.html")
-
-
 
 #added route of the chat panel
 @app.route("/chat")
