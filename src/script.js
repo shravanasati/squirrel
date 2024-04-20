dd49fef262c41af6589a70fcee88b555330dfe8a
OmKansara <okansara8@gmail.com>
2024-04-20 19:28:24 +0530

    updated script for button

--- src/script.js
+++ src/script.js
@@ -1,1 +1,11 @@
 console.log("am am am no sex noooo cyka blyat");
+
+sendButton.eventListener("click", ()=>{
+    question = document.getElementById("question").value;
+    document.getElementById("question").value = "";
+    
+    question.InnerHTML = questionInput;
+
+    postData("/api",{"question":questionInput});
+
+})
--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,1 +1,1 @@
-console.log("am am am no sex noooo cyka blyat");
+console.log("am am am no sex noooo cyka blyat"),sendButton.eventListener("click",(()=>{question=document.getElementById("question").value,document.getElementById("question").value="",question.InnerHTML=questionInput,postData("/api",{question:questionInput})}));
